2d12c3674f5d31e3cb3cd1409b663d94f652d8dd
000raihan <000raihanahmad@gmail.com>
2023-11-20 08:23:31 +0600

    initial specialyt done

--- src/components/profesionlItems.js/Specailtiy.js
+++ src/components/profesionlItems.js/Specailtiy.js
@@ -1,36 +1,81 @@
-import React from "react";
-import { Form, Select } from "antd";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import debounce from "lodash/debounce";
+import { Select, Spin } from "antd";
+import { useGetAllSpecialtyQuery } from "../../redux/features/speciality/speciality";
 
-const Specailtiy = () => {
+function DebounceSelect({ fetchOptions, debounceTimeout = 800, ...props }) {
+  const [fetching, setFetching] = useState(false);
+  const [options, setOptions] = useState([]);
+  const fetchRef = useRef(0);
+  const debounceFetcher = useMemo(() => {
+    const loadOptions = (value) => {
+      fetchRef.current += 1;
+      const fetchId = fetchRef.current;
+      setOptions([]);
+      setFetching(true);
+      fetchOptions(value).then((newOptions) => {
+        if (fetchId !== fetchRef.current) {
+          // for fetch callback order
+          return;
+        }
+        setOptions(newOptions);
+        setFetching(false);
+      });
+    };
+    return debounce(loadOptions, debounceTimeout);
+  }, [fetchOptions, debounceTimeout]);
   return (
-    <div>
-      {" "}
-      <Form.Item
-        style={{ marginBottom: "10px" }}
-        // label={<p style={{ margin: 0, padding: 0 }}>Gender : </p>}
-        name="speciality"
-        labelCol={{ span: 1 }}
-        wrapperCol={{ span: 6 }}
-        // rules={[{ required: true, message: "Please enter your name!" }]}
-      >
-        <Select
-          mode="multiple"
-          allowClear
-          style={{
-            width: "100%",
-          }}
-          placeholder="Speciality"
-          defaultValue={"Cardiologist"}
-          // onChange={handleChange}
-          options={[
-            { label: "Cardiologist", value: "Cardiologist" },
-            { label: "Neurologist", value: "Neurologist" },
-            { label: "Gastroentrologist", value: "Gastroentrologist" },
-          ]}
-        />
-      </Form.Item>
-    </div>
+    <Select
+      labelInValue
+      filterOption={false}
+      onSearch={debounceFetcher}
+      notFoundContent={fetching ? <Spin size="small" /> : null}
+      {...props}
+      options={options}
+    />
+  );
+}
+
+// Usage of DebounceSelect
+
+async function fetchUserList(username) {
+  console.log("fetching user", username);
+  return fetch("https://randomuser.me/api/?results=5")
+    .then((response) => response.json())
+    .then((body) =>
+      body.results.map((user) => ({
+        label: `${user.name.first} ${user.name.last}`,
+        value: user.login.username,
+      }))
+    );
+}
+
+const Speciality = ({ doctor, success, error, warning }) => {
+  const [value, setValue] = useState([]);
+
+  const [specialities, setSpecialities] = useState([]);
+
+  const { data: getSpeciality } = useGetAllSpecialtyQuery();
+
+  useEffect(()=>{
+    if(getSpeciality?.data){
+        setSpecialities(getSpeciality?.data)
+    }
+  },[getSpeciality])
+
+  return (
+    <DebounceSelect
+      mode="multiple"
+      value={value}
+      placeholder="Select users"
+      fetchOptions={specialities}
+      onChange={(newValue) => {
+        setValue(newValue);
+      }}
+      style={{
+        width: "100%",
+      }}
+    />
   );
 };
-
-export default Specailtiy;
+export default Speciality;
