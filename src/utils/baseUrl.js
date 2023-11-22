c122cfcf9589c70667c9db72d2c4c7be58d0a7f9
000raihan <000raihanahmad@gmail.com>
2023-11-22 13:29:17 +0600

    base url changed

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,5 @@
-const baseUrl = "http://localhost:6049";
-// const baseUrl = "http://116.68.200.97:6049";
+// const baseUrl = "http://localhost:6049";
+const baseUrl = "http://116.68.200.97:6049";
 // const baseUrl = "http://172.16.16.126:6070"
 
 export default baseUrl
